dfe3bf5698139699fe6648cc91cbb8a71c373369
Diep <diepani303@gmail.com>
2025-07-31 00:52:38 +0700

    Add for Note background: color and image

---
+++ app/src/main/java/com/example/todo_listv2/adapters/ColorBackgroundAdapter.java
@@ -0,0 +1,74 @@
+package com.example.todo_listv2.adapters;
+
+import static java.security.AccessController.getContext;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.todo_listv2.R;
+
+import java.util.List;
+
+public class ColorBackgroundAdapter extends RecyclerView.Adapter<ColorBackgroundAdapter.ViewHolder> {
+    public interface OnItemClickListener {
+        void onItemClick(int colorOrDrawable);
+    }
+
+    private final List<Integer> items;
+    private final boolean isColorMode;
+    private final OnItemClickListener listener;
+
+    public ColorBackgroundAdapter(List<Integer> items, boolean isColorMode, OnItemClickListener listener) {
+        this.items = items;
+        this.isColorMode = isColorMode;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_circle_selector, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        int item = items.get(position);
+        if (isColorMode) {
+            holder.imageView.setImageDrawable(null); // clear image
+            GradientDrawable circle = new GradientDrawable();
+            circle.setShape(GradientDrawable.OVAL);
+            circle.setColor(item);
+            holder.imageView.setBackground(circle);
+        } else {
+            holder.imageView.setBackground(null);
+            Drawable drawable = ContextCompat.getDrawable(holder.itemView.getContext(), item);
+            holder.imageView.setImageDrawable(drawable);
+        }
+
+        holder.itemView.setOnClickListener(v -> listener.onItemClick(item));
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.item_image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
--- app/src/main/java/com/example/todo_listv2/fragments/AddNoteFragment.java
+++ app/src/main/java/com/example/todo_listv2/fragments/AddNoteFragment.java
@@ -3,6 +3,8 @@ package com.example.todo_listv2.fragments;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,16 +12,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.todo_listv2.R;
+import com.example.todo_listv2.adapters.ColorBackgroundAdapter;
 import com.example.todo_listv2.databinding.FragmentAddNewNoteBinding;
 import com.example.todo_listv2.models.Note;
 import com.example.todo_listv2.viewModels.NotesViewModel;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AddNoteFragment extends DialogFragment {
     private FragmentAddNewNoteBinding binding;
@@ -30,6 +42,11 @@ public class AddNoteFragment extends DialogFragment {
     private View microButton;
     private CardView selectColorButton;
     private EditText titleText, contentText;
+    private ColorBackgroundAdapter colorBackgroundAdapter;
+    private RecyclerView colorRecycler, backgroundRecycler;
+    private LinearLayout fragmentRoot;
+    private int backgroundId = 0;
+    private String colorSelected = "";
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState){
@@ -55,6 +72,8 @@ public class AddNoteFragment extends DialogFragment {
         selectColorButton = binding.selectColorButton;
         titleText = binding.titleNoteEdit;
         contentText = binding.contentNoteEdit;
+
+        fragmentRoot = binding.fragmentAddNewNote;
     }
 
     private void setOnClickListener(){
@@ -68,7 +87,7 @@ public class AddNoteFragment extends DialogFragment {
         });
 
         selectColorButton.setOnClickListener(v -> {
-
+            showColorBackgroundSelector();
         });
     }
 
@@ -77,9 +96,74 @@ public class AddNoteFragment extends DialogFragment {
         String content = contentText.getText().toString();
 
         if(!content.isEmpty()){
-            Note newNote = new Note(title, content, "", 0, userId);
+            Note newNote = new Note(title, content, colorSelected, backgroundId, userId);
             Log.d("NOTE", newNote.toString());
             notesViewModel.saveNewNote(newNote);
         }
+    }
+    private void showColorBackgroundSelector(){
+        BottomSheetDialog dialog = new BottomSheetDialog(requireContext());
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.dialog_bottom_color_background_selector, null);
+        dialog.setContentView(view);
+
+        colorRecycler = view.findViewById(R.id.colorRecyclerView);
+        backgroundRecycler = view.findViewById(R.id.backgroundRecyclerView);
+
+        String[] colorHexList = {"#75f4f4", "#90e0f3", "#b8b3e9", "#d999b9", "#d17b88", "#f49097", "#dfb2f4", "#f5e960", "#f2f5ff", "#55d6c2"};
+        List<Integer> colorList = new ArrayList<>();
+        for (String hex : colorHexList) {
+            colorList.add(Color.parseColor(hex));
+        }
+        List<Integer> backgrounds = Arrays.asList(
+                R.drawable.none_bg,
+                R.drawable.bg_1,
+                R.drawable.bg_2,
+                R.drawable.bg_3,
+                R.drawable.bg_4,
+                R.drawable.bg_5,
+                R.drawable.bg_6,
+                R.drawable.bg_7,
+                R.drawable.bg_8,
+                R.drawable.bg_9,
+                R.drawable.bg_10);
+
+
+
+        colorRecycler.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        colorRecycler.setAdapter(new ColorBackgroundAdapter(colorList, true, selectedColor -> {
+            fragmentRoot.setBackgroundTintList(ColorStateList.valueOf(selectedColor));
+            int index = colorList.indexOf(selectedColor);
+
+            colorSelected = colorHexList[index];
+        }));
+
+        backgroundRecycler.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        backgroundRecycler.setAdapter(new ColorBackgroundAdapter(backgrounds, false, selectedDrawable -> {
+            fragmentRoot.setBackgroundTintList(null);
+            if(selectedDrawable == R.drawable.none_bg){
+                fragmentRoot.setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                setColorTint(Color.BLACK);
+                backgroundId = 0;
+            } else {
+                fragmentRoot.setBackgroundResource(selectedDrawable);
+                int index = backgrounds.indexOf(selectedDrawable);
+                if(index == 4 || index == 6) {
+                    setColorTint(Color.BLACK);
+                } else {
+                    setColorTint(Color.WHITE);
+                }
+                backgroundId = index;
+            }
+
+
+        }));
+
+        dialog.show();
+    }
+
+    private void setColorTint(int color){
+        contentText.setTextColor(ColorStateList.valueOf(color));
+        titleText.setTextColor(ColorStateList.valueOf(color));
+        backButton.setImageTintList(ColorStateList.valueOf(color));
     }
 }
