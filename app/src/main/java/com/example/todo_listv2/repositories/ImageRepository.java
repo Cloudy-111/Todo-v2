203c7c7abfecff4ea0e00e7c246ab08f5566edaf
Diep <diepani303@gmail.com>
2025-08-02 21:43:38 +0700

    ENV

---
+++ app/src/main/java/com/example/todo_listv2/repositories/ImageRepository.java
@@ -0,0 +1,16 @@
+package com.example.todo_listv2.repositories;
+
+import android.content.Context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImageRepository {
+    private static boolean isInitialized = false;
+
+    public static void initCloudinary(Context context){
+        if(!isInitialized){
+            Map<String, String> config = new HashMap<>();
+        }
+    }
+}
